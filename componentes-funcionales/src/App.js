0240960c1df6d0048f4ac41dacecdb645cd68ef9
Federico Carranza Zavalia <fedecarranzanz@gmail.com>
2023-09-19 15:09:01 -0300

    func comp

--- componentes-funcionales/src/App.js
+++ componentes-funcionales/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Container from '@mui/material/Container';
 import FormSignUp from './components/FormSignUp';
+import { Typography } from '@mui/material';
+
 function App() {
   return (
-    <>
-    <h1>Formulario  Resgistro</h1>
+    <Container component="section" maxWidth="sm">
+    <Typography variant="h3" align="center">Formulario  Resgistro</Typography>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
--- componentes-funcionales/src/components/FormSignUp.jsx
+++ componentes-funcionales/src/components/FormSignUp.jsx
@@ -1,17 +1,64 @@
+import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 
 function FormSignUp() {
+    const [name, setName] = useState("")
+    useEffect(() => {
+
+    }, [name])
     return <form>
-        <label>Nombre</label>
-        <input type="text" />
-        <label>Apellidos</label>
-        <input type="text" />
-        <label>Correo electronico</label>
-        <input type="email" />
-        <label>Promociones</label>
-        <input type="checkbox" />
-        <label>Noveddess</label>
-        <input type="checkbox" />
+        <TextField
+            id="name"
+            label="nombre"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            onChange={(e) => {
+                console.log(e.target.value)
+                setName(e.target.value)
+            }}
+            value={name}
+        />
+
+
+        <TextField
+            id="lastName"
+            label="Apellidos"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+
+        />
+
+        <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+
+        />
+
+
+
+        <FormGroup>
+            <FormControlLabel control={<Switch
+                defaultChecked />
+            }
+                label="Promociones"
+            />
+
+            <FormControlLabel
+                control={<Switch defaultChecked />
+                }
+                label="Novedades"
+            />
+        </FormGroup>
         <Button variant="contained">
             Registrarse
         </Button>
